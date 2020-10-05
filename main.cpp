d6a688b29089df8a267a543d1a7e4a6307935635
angel valdivia <angel.valdivia1336@alumnos.udg.mx>
2020-10-05 17:21:52 -0500

    prueba 01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    unsigned int base;
+    unsigned int altura;
+
+    cin >> base;
+    cin >> altura;
+
+    cout << (base * altura)/2 << endl;
 
     return 0;
 }
